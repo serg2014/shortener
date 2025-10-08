1cb8062439b9997343d4961868d3edc6bb161dd4
Serg K <kozunov@example.com>
2025-10-09 02:50:00 +0300

    right way for shutdown

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,11 +4,15 @@ import (
 	"bytes"
 	"compress/gzip"
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"os/signal"
 	"slices"
 	"strings"
 	"sync"
+	"syscall"
+	"time"
 
 	"go.uber.org/zap"
 
@@ -128,10 +132,58 @@ func run() error {
 	defer store.Close()
 
 	app := app.NewApp(store)
-	for i := 0; i < poolSize; i++ {
-		go app.DeleteUserURLsBackground(ctx)
+
+	srv := http.Server{
+		Addr:    fmt.Sprintf("%s:%d", config.Config.Host, config.Config.Port),
+		Handler: Router(app),
 	}
 
+	var wg sync.WaitGroup
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for i := 0; i < poolSize; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			app.DeleteUserURLsBackground(ctx)
+			logger.Log.Info("Stop delete gorutine")
+		}()
+	}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		// создаем контекст, который будет отменен при получении сигнала
+		ctxS, stop := signal.NotifyContext(ctx, syscall.SIGINT, syscall.SIGTERM)
+		defer stop()
+
+		select {
+		// 	ждем сигнала от ОС
+		case <-ctxS.Done():
+			logger.Log.Info("catch signal")
+		// ждем отмены контекста
+		case <-ctx.Done():
+			logger.Log.Info("stop")
+		}
+
+		ctxT, cancelT := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancelT()
+		if err := srv.Shutdown(ctxT); err != nil {
+			logger.Log.Info("Server forced to shutdown", zap.Error(err))
+		}
+	}()
+
 	logger.Log.Info("Running server", zap.String("address", config.Config.String()), zap.String("storage", fmt.Sprintf("%T", store)))
-	return http.ListenAndServe(fmt.Sprintf("%s:%d", config.Config.Host, config.Config.Port), Router(app))
+	err = srv.ListenAndServe()
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		logger.Log.Panic("error in ListenAndServe", zap.Error(err))
+	}
+
+	// отменяем контекст, чтобы завершить горутины
+	cancel()
+
+	wg.Wait()
+	logger.Log.Info("Server is shutdown")
+	return nil
 }

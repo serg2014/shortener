79b057c760b71860f9ea36e2801393a64516d7a8
Serg K <kozunov@example.com>
2025-11-10 22:18:05 +0300

    stupid

--- internal/storage/database.go
+++ internal/storage/database.go
@@ -232,7 +232,7 @@ func (storage *storageDB) DeleteUserURLS(ctx context.Context, data models.Reques
 
 // InternalStats get stat
 func (storage *storageDB) InternalStats(ctx context.Context) (*models.InternalStats, error) {
-	query := `SELECT count(short_url) count(distinct(user_id)) FROM short2orig`
+	query := `SELECT count(short_url), count(distinct(user_id)) FROM short2orig`
 	row := storage.db.QueryRowContext(ctx, query)
 	rv := models.InternalStats{}
 	err := row.Scan(&rv.Urls, &rv.Users)

7699e091044bd21f61c25af333278dde887c552f
Serg K <kozunov@example.com>
2025-11-11 19:37:42 +0300

    go:generate protoc

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,5 +1,7 @@
 package main
 
+//go:generate protoc --go_out=. --go_opt=paths=source_relative --go-grpc_out=. --go-grpc_opt=paths=source_relative proto/short.proto
+
 import (
 	"compress/gzip"
 	"context"

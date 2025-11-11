044182e5d05a4ee3627138e4f88bb3caa05239d5
Serg K <kozunov@example.com>
2025-11-11 18:17:16 +0300

    GetURL

--- cmd/shortener/grpc_server.go
+++ cmd/shortener/grpc_server.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	pb "github.com/serg2014/shortener/cmd/shortener/proto"
@@ -9,6 +10,7 @@ import (
 	"github.com/serg2014/shortener/internal/auth"
 	"github.com/serg2014/shortener/internal/logger"
 	"github.com/serg2014/shortener/internal/models"
+	"github.com/serg2014/shortener/internal/storage"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -38,7 +40,7 @@ func (s *GrpcServer) InternalStats(ctx context.Context, request *pb.InternalStat
 		// TODO нужен логер чтобы сам делала префикс grpc
 		logger.Log.Error("InternalStats", zap.Error(err))
 		code := codes.Internal
-		return nil, status.Error(codes.Internal, code.String())
+		return nil, status.Error(code, code.String())
 	}
 	return &pb.InternalStatsResponse{
 		Urls:  uint32(data.Urls),
@@ -53,7 +55,7 @@ func (s *GrpcServer) Ping(ctx context.Context, request *pb.PingRequest) (*pb.Pin
 	if err := s.app.Ping(ctx); err != nil {
 		logger.Log.Error("ping", zap.Error(err))
 		code := codes.Internal
-		return nil, status.Error(codes.Internal, code.String())
+		return nil, status.Error(code, code.String())
 	}
 	return &pb.PingResponse{}, nil
 }
@@ -63,15 +65,36 @@ func (s *GrpcServer) DeleteUserURLS(ctx context.Context, request *pb.DeleteUserU
 	if err != nil {
 		logger.Log.Error("can not find userid", zap.Error(err))
 		code := codes.Internal
-		return nil, status.Error(codes.Internal, code.String())
+		return nil, status.Error(code, code.String())
 	}
 	req := models.RequestForDeleteURLS(request.Shorts)
 	err = s.app.DeleteUserURLS(ctx, req, userID)
 	if err != nil {
 		logger.Log.Error("can not find userid", zap.Error(err))
 		code := codes.Internal
-		return nil, status.Error(codes.Internal, code.String())
+		return nil, status.Error(code, code.String())
 	}
 
 	return &pb.DeleteUserURLSResponse{}, nil
+}
+
+func (s *GrpcServer) GetURL(ctx context.Context, request *pb.GetURLRequest) (*pb.GetURLResponse, error) {
+	origURL, ok, err := s.app.Get(ctx, request.Short)
+	var code codes.Code
+	if err != nil {
+		switch {
+		case errors.Is(err, storage.ErrDeleted):
+			// TODO http.StatusGone
+			code = codes.Internal
+		default:
+			code = codes.Internal
+		}
+		logger.Log.Error("error in a.Get", zap.Error(err))
+		return nil, status.Error(code, code.String())
+	}
+	if !ok {
+		return nil, status.Error(codes.InvalidArgument, "bad short")
+	}
+
+	return &pb.GetURLResponse{Url: origURL}, nil
 }
